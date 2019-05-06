b4cb32ec1f71b287f16b6fec6f46a4f825bf669a
Anton Sozonov <unboundnomad@gmail.com>
2019-05-06 12:49:29 +0400

    kinda done

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import entity.Entity;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Entity entity = new Entity();
+
+        Arrays.stream(entity.getClass().getDeclaredMethods())
+                .peek(System.out::println).
+                forEach(e -> Arrays.stream(e.getAnnotations())
+                        .forEach(System.out::println));
+    }
+
+}
---
+++ src/main/java/entity/Entity.java
@@ -0,0 +1,23 @@
+package entity;
+
+import annotations.SecondAnnotation;
+import annotations.Secured;
+
+public class Entity {
+
+    @Secured(1)
+    public void firstMethod () {
+
+    }
+
+    @SecondAnnotation
+    @Secured(value = 2, property = "weak")
+    private void secondMethod () {
+
+    }
+
+    public void thirdMethod() {
+
+    }
+
+}
